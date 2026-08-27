971f8c37780efca8f57b08044c5385328c3a18a0
Rumeshbharath Ramanathan <rumeshbharathramanathan@gmail.com>
2026-08-27 17:36:33 +0530

    sencound

--- JavaPracticeSpace/src/com/javapractice/com/JavaPractice.java
+++ JavaPracticeSpace/src/com/javapractice/com/JavaPractice.java
@@ -4,7 +4,27 @@ public class JavaPractice {
 
 	public static void main(String[] args) {
 
-		System.out.println("jeeva");
+		int arr[]= {10,15,20,8,20,15,45};
+		
+		int first=0;
+		int sec=0;
+		
+		
+		for (int i=0;i<arr.length;i++) {
+			
+			
+			if (first>arr[i]) {
+				first=arr[i];
+				sec=first;
+			}else if (sec>arr[i]) {
+				
+				sec=arr[i];
+			}
+			
+		}
+		
+		System.out.println("Sec Output "+sec);
+		
 	}
 
 }
