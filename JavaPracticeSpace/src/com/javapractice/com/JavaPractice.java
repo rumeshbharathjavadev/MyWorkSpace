ee84dcf1b862ad3117c7ba28579d93889a739e2b
Rumeshbharath Ramanathan <rumeshbharathramanathan@gmail.com>
2026-08-31 18:25:40 +0530

    next

--- JavaPracticeSpace/src/com/javapractice/com/JavaPractice.java
+++ JavaPracticeSpace/src/com/javapractice/com/JavaPractice.java
@@ -1,27 +1,14 @@
 package com.javapractice.com;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 public class JavaPractice {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 20, 30, 20, 40, 50, 10, 30 };
-
-		Set<Integer> hs = new LinkedHashSet<>();
-
-		for (int i = 0; i < arr.length; i++) {
-
-			if(!hs.add(arr[i])){
-				hs.add(arr[i]);
-			}
-		}
 		
-		for (int a:hs) {
-			System.out.println(a);
-		}
-
+		String str1 = "listen";
+		String str2 = "silent";
+		
+		
 	}
 
 }
